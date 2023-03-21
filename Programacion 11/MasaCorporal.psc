1843656be5634c1c91f810da0274804d5d0df145
sarithpinto <124885645+sarithpinto@users.noreply.github.com>
2023-03-21 10:31:32 -0500

    Add files via upload

---
+++ Programacion 11/MasaCorporal.psc
@@ -0,0 +1,9 @@
+Algoritmo MasaCorporal 
+	Definir kg Como Real 
+	Definir m Como Real 
+	Escribir "Digite su peso en kg" 
+	Leer kg 
+	Escribir "Digite su altura en metros" 
+	Leer m 
+	Escribir "su indice de masa corporal es" , (kg/(m)^2)
+FinAlgoritmo
---
+++ mimimi/app.js
@@ -0,0 +1,7 @@
+function sumar() {
+	var num1 = parseInt(document.getElementById("num1").value);
+	var num2 = parseInt(document.getElementById("num2").value);
+	var resultado = num1 + num2;
+	document.getElementById("resultado").value = resultado;
+	return false;
+}
